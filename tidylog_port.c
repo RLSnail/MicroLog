35e90e0f043d6a837c8e53440dc969be83913fb4
RLSnail <13355365693@qq.com>
2024-05-15 15:18:14 +0800

    实现了一个空的接口函数，留给用户具体实现

---
+++ tidylog_port.c
@@ -0,0 +1,11 @@
+#include "tidylog_port.h"
+
+/**
+ * @brief This file contains some interface functions which coupled to the platform
+ * you used. The interface function `__tidylog_output` is required to implement, but
+ * the others are not mandatory to finish.
+*/
+
+void __tidylog_output(char* content, unsigned long long len) {
+
+}
---
+++ tidylog_port.h
@@ -0,0 +1,6 @@
+#ifndef __TIDYLOG_PORT_H
+#define __TIDYLOG_PORT_H
+
+void __tidylog_output(char* content, unsigned long long len);
+
+#endif
