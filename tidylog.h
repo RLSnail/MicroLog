fbc09144bca641ac725a0f55797db5e479e57ab5
RLSnail <13355365693@qq.com>
2024-05-16 12:45:11 +0800

    feat: 添加了一个用于控制是否使用内联优化的宏

--- tidylog.h
+++ tidylog.h
@@ -1,4 +1,16 @@
 #ifndef __TIDYLOG_H
 #define __TIDYLOG_H
 
+#include "tidylog_port.h"
+
+/**
+ * @brief  macro indicates wether using the symbol `inline` to optimize a function.
+*/
+#define TIDYLOG_USE_INLINE 1
+#if TIDYLOG_USE_INLINE
+    #define __TIDYLOG_INLINE inline
+#else
+    #define __TIDYLOG_INLINE
+#endif
+
 #endif
